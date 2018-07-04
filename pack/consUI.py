3ce140ee1f6a353296f58d15bd045db792bfee9f
Maksim42 <maksim.lomako@gmail.com>
2018-07-04 18:06:52 +0300

    Add MenuList

--- pack/consUI.py
+++ pack/consUI.py
@@ -1,58 +1,87 @@
-def Input(headMesage):
-    """Standart console command input"""
-    return input(headMesage + '>')
+"""Console UI elements"""
+def read(message):
+    """Cconsole command input"""
+    return input("{}>".format(message))
 
-def Output(message):
-    print('\n{}\n'.format(message))
+def write(message):
+    """Console write"""
+    print("\n{}\n".format(message))
 
-def ConfirmDialog(message):
+def confirm(message):
     """Show confirm dialog"""
-    result = None
-    while result is None:
-        print(message + "(y/n):", end=' ')
-        answer = input().lower()
-        if answer in ['y', "yes"]:
-            result = True
-        elif answer in ['n', "no"]:
-            result = False
-        else:
-            print("Uncorect input")
-    return result
+    positive = ['y', "yes"]
+    negative = ['n', "no"]
+    result = [None]
+    def set_res(arg):
+        result[0] = arg
+    promt = CommandPromt(message, single=True)
+    promt.add_command(positive, lambda: set_res(True))
+    promt.add_command(negative, lambda: set_res(False))
+    promt.show()
+    return result[0]
 
-class ComandLine:
-    """Console menu"""
-    def __init__(self, head='', closeAlias=None):
-        self.headMes = head
-        self.commands = []
-        self.exitCommands = []
-        self.showing = True
-        if not closeAlias is None:
-            self.AddExit(closeAlias)
 
-    def Show(self):
-        while self.showing:
-            command = Input(self.headMes)
-            if command in self.exitCommands:
-                self.Exit()
-                break
-            for c in self.commands:
-                if command in c[0]:
-                    c[1]()
+class MenuList:
+    """Menu with some options"""
+    def __init__(self, name=''):
+        self.name = name
+        self._command_line = CommandPromt(self.name, single=True)
+        self._option_names = ''
+        self._option_num = 0
+
+    def show(self):
+        """Show menu"""
+        write(self._option_names)
+        self._command_line.show()
+
+    def add_option(self, name, delegate):
+        """Add option to menu"""
+        self._option_num += 1
+        num = self._option_num
+        self._command_line.add_command((str(num),), delegate)
+        if self._option_names != '':
+            self._option_names += '\n'
+        self._option_names += "{0}. {1}".format(num, name)
+
+
+class CommandPromt:
+    """Command line input"""
+    def __init__(self, head='', close_alias=None, single=False):
+        self._head_message = head
+        self._commands = []
+        self._exit_commands = []
+        self._showing = not single
+        if close_alias is not None:
+            self._add_exit(close_alias)
+
+    def show(self):
+        """Begin input in command promt"""
+        while True:
+            right_command = False
+            while not right_command:
+                command = read(self._head_message)
+                if command in self._exit_commands:
+                    right_command = True
+                    self._exit()
                     break
-            else:
-                self.UnknowCommand()
+                for c in self._commands:
+                    if command in c[0]:
+                        right_command = True
+                        c[1]()
+                        break
+                else:
+                    write('Unsupported command!')
+            if not self._showing:
+                break
 
-    def AddCommand(self, alias, delegate):
+    def add_command(self, alias, delegate):
         """Add command"""
-        self.commands.append((alias, delegate))
+        self._commands.append((alias, delegate))
 
-    def AddExit(self, alias):
-        self.exitCommands = alias
+    def _add_exit(self, alias):
+        """Add exit alias"""
+        self._exit_commands += alias
 
-    def UnknowCommand(self):
-        print('Unsupported command!')
-
-    def Exit(self):
-        self.showing = False
-    
-
+    def _exit(self):
+        """Close command promt"""
+        self._showing = False
